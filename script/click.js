66917401395cee5037cfa6345c873328002c10d1
gftgrrferty <gftgrrferty@gmail.com>
2026-07-03 18:31:16 +0900

    クリックしたら画像が変わるスクリプトを作成した

---
+++ script/click.js
@@ -0,0 +1,12 @@
+let flag = false;
+const profileImage = document.getElementById("profile_image");
+
+profileImage.addEventListener("click", function() {
+  if (flag) {
+    profileImage.src = "./img/VRChat_2026-05-29_19-59-43.183_1920x1080.png";
+    flag = false;
+  } else {
+    profileImage.src = "./img/VRChat_2026-04-20_20-26-52.706_2048x1440.png";
+    flag = true;
+  }
+});
