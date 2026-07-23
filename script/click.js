ea6c23b59ed5d7c8c1b9ae8292e29789238f2a63
あび茶 <137599113+gftgrrferty@users.noreply.github.com>
2026-07-23 14:22:28 +0900

    画像変更

--- script/click.js
+++ script/click.js
@@ -7,7 +7,7 @@
       profileImage.src = "./img/VRChat_2026-05-29_19-59-43.183_1920x1080.png";
       flag = false;
     } else {
-      profileImage.src = "./img/VRChat_2026-04-20_20-26-52.706_2048x1440.png";
+      profileImage.src = "./img/kaguyabaka.png";
       flag = true;
     }
   });
